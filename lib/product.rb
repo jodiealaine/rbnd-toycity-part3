e7b589b25c2271faf88fb22ae7b6da56fe5b261d
Jodie Alaine Parker <me@jodiealaine.com>
2016-02-17 01:12:56 +0000

    Add ability to prevent duplicate products being added

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,19 @@
 class Product
+	attr_reader :title
 	@@products = []
 
-	def initialize opts={}
-		@@products << self
+	def initialize options={}
+		@title = options[:title]
+		add_to_products
 	end
 
 	def self.all
 		@@products
+	end
+
+	private
+
+	def add_to_products
+		@@products.any? {|t| t.title.include?(@title)} ? (raise DuplicateProductError, "#{@title} already exists.") : @@products << self 
 	end
 end
