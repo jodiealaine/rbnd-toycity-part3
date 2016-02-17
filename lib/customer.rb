fe8c898cb00703c5568328d341aba691c4bd35cf
Jodie Alaine Parker <me@jodiealaine.com>
2016-02-17 13:23:00 +0000

    Add ability to get the number of customers

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,5 +1,20 @@
 class Customer
+	attr_reader :name
+	
+	@@customers = []
+	
 	def initialize options={}
 		@name = options[:name]
+		add_to_customers
+	end
+
+	def self.all
+    @@customers
+	end
+
+	private
+
+	def add_to_customers
+		@@customers << self
 	end
 end
